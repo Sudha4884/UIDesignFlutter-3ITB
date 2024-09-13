60aad579a948645e729914c1c1307d9986262fec
Anamitra Joshi <120377524+anamitrajoshi@users.noreply.github.com>
2024-09-13 11:23:51 +0530

    Create row_layout.dart

---
+++ 1275/flutter/row_layout.dart
@@ -0,0 +1,39 @@
+  import 'package:flutter/material.dart';
+
+  void main() {
+    runApp(const MyApp());
+  }
+
+  class MyApp extends StatelessWidget{
+    const MyApp({super.key});
+    @override
+    Widget build(BuildContext context){
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Row Layout Example'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.red,
+                child: Text('Monika'),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.blue,
+                child: Text('Anamitra'),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.green,
+                child: Text('Harshitha'),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
